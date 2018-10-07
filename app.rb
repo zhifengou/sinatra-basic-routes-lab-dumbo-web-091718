a4c5a6b1fc3cba2bda611bcd36f9039844b4be57
zhifeng ou <zhifengou@hotmail.com>
2018-10-07 20:40:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  
+  get '/name' do 
+    "My name is Zhifeng."
+  end
+  
+  get ('/hometown') { "My hometown is Jiangmen,China"}
+  get ('/favorite-song') {"My favorite song is 300"}
+  end
